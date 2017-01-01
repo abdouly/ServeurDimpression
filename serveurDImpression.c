69ff2aebe2a73cf67b868723ecc3537ce8bf0435
telly <teliwal91@gmail.com>
2017-01-01 18:52:10 +0100

    server

--- serveurDImpression.c
+++ serveurDImpression.c
@@ -13,13 +13,19 @@ typedef struct imprimante{
   int type;       //contient le type de l'imprimante. 0 : locale, 1 : distance
 }Imprimante;
 
+typedef struct infos_serveur
+{
+	char nom[20];
+	int numServeur;
+}Infos_serveur;
+
 //initialiser le serveur pour qu'il recoive les requetes
 int init_serveur(char *serveur){
 	int numServeur;
     unlink(serveur);
     if ((numServeur = initialiserServeur(serveur)) < 0)
   	{
-	  fprintf(stderr, "Erreur initialiserServeur: %s: %s\n",
+	  	fprintf(stderr, "Erreur initialiserServeur: %s: %s\n",
 			  serveur, messageErreur(numServeur));
 	  	exit(1);
   	}
@@ -28,68 +34,76 @@ int init_serveur(char *serveur){
 
 //fonction du scheduler
 void * cups_scheduler(void *args){
-	/*int numCommunication;
+	int numCommunication,numServeur;
+	char serveur[20];
+	Infos_serveur * infos = (Infos_serveur *) args;
+	numServeur = infos->numServeur;
+	strcpy(serveur,infos->nom);
+	printf("[ Scheduler ] demarrage OK %d\n",numServeur);
 	for(;;){
 		if((numCommunication = accepterCommunication(numServeur)) < 0){
 		 	fprintf(stderr, "Erreur accepterCommunication: %s: %s\n",serveur, messageErreur(numCommunication));
 	 	 	exit(1);
 	   	}
-         lire le nom du fichier à transferer
-	}*/
-	printf("Scheduler demarre\n");
-	for(;;);
+	}
 }
 
 //fonction d'un cups filter
 void * cups_filter(void *args){
-   printf("Filter demarre\n");
+   printf("[ Filter ] demarrage OK\n");
    for(;;){
 
    }
 }
 //fonction d'un imprimante locale
 void * imprimante_locale(void *args){
- 	printf("Imprimante locale demarre\n");
+	char fichier_imprimante[20];
+	char fichier;
+ 	printf("[ Imprimante locale ] demarrage OK\n");
  	for(;;){
  		
  	}
 }
 
 //fonctions qui demarre les threads des imprimantes locales
-void demarrer_imprimantes_locales(Imprimante *imprimantes,int nb_imprimantes,pthread_t *idThdImp){
+void demarrer_imprimantes_locales(Imprimante *imprimantes,int nb_imprimantes,pthread_t *idThdImp,int *nb_imprimantes_locale){
 	int i,etat,j;
 	j = 0;
 	Imprimante p;
+	printf("[ Main ] demarrage Imprimantes locales\n");
 	for(i=0; i < nb_imprimantes; i++){
 		p = imprimantes[i];
 		if(p.type == 0){
 			if((etat=pthread_create(&idThdImp[j++], NULL, imprimante_locale,NULL)) != 0){
-				fprintf(stderr, "Echec demarrage imprimante\n");
+				fprintf(stderr, "[ Main ] Echec demarrage Imprimante locale\n");
 				exit(1);
 			}
 		}
 	}
-	printf("Fin demarrage imprimantes locales\n");
+	*nb_imprimantes_locale = j;
+	sleep(1);
 }
 //fonctions qui demarres les threads des cupsFilters
 void demarrer_filters(int nb_filters,pthread_t *idThdFil){
 	int i,etat;
+	printf("[ Main ] demarrage Filters\n");
 	for(i = 0; i < nb_filters; i++){
 		if((etat=pthread_create(&idThdFil[i], NULL, cups_filter,NULL)) != 0){
-				fprintf(stderr, "Echec demarrage Filter\n");
-				exit(1);
+			fprintf(stderr, "[ Main ] Echec demarrage Filters\n");
+			exit(1);
 		}
 	}
-	printf("Fin demarrage filters\n");	
+	sleep(1);	
 }
 //fonction qui demarre le scheduler
-void demarrer_scheduler(pthread_t idScheduler){
+void demarrer_scheduler(pthread_t *idScheduler,Infos_serveur infos){
  	int etat;
- 	if((etat=pthread_create(&idScheduler, NULL, cups_scheduler,NULL)) != 0){
-		fprintf(stderr, "Echec demarrage scheduler\n");
+ 	printf("[ Main ] demarrage Scheduler\n");
+ 	if((etat=pthread_create(idScheduler, NULL, cups_scheduler,&infos)) != 0){
+		fprintf(stderr, "[ Main ] Echec demarrage scheduler\n");
 		exit(1);
 	}
-	printf("Fin demarrage scheduler\n");
+	sleep(1);
 }
 
 void lectureConfiguration(char *fileName, char *serverName, Imprimante **imprimantes,int *nb_imprimantes) {
@@ -142,14 +156,30 @@ int main(int argc,char *argv[]){
 	pthread_t idScheduler;
 	pthread_t idThdImp[10];
 	pthread_t idThdFil[10];
+	int numServeur,etat;
+	Infos_serveur infos;
 	Imprimante * imprimantes = NULL;
-	int nb_imprimantes;
+	int nb_imprimantes,nbFilters,i,nb_imprimantes_locale;
+	nbFilters = 4;
 	char serveur[20];
+	printf("[ Main ] Initialisation du serveur d'impression\n");
 	lectureConfiguration(argv[1],serveur,&imprimantes,&nb_imprimantes);
-	demarrer_scheduler(idScheduler);
-	demarrer_filters(4,idThdFil);
-	if(imprimantes == NULL)
-		printf("Oulalalalalalal\n");
-	demarrer_imprimantes_locales(imprimantes,nb_imprimantes,idThdImp);
-	for(;;);
+	numServeur = init_serveur(serveur);
+	infos.numServeur = numServeur;
+	strcpy(infos.nom,serveur);
+	demarrer_scheduler(&idScheduler,infos);
+	demarrer_filters(nbFilters,idThdFil);
+	demarrer_imprimantes_locales(imprimantes,nb_imprimantes,idThdImp,&nb_imprimantes_locale);
+	printf("[ Main ] Initialisation serveur OK\n");
+	if((etat = pthread_join(idScheduler,NULL)) != 0)
+		fprintf(stderr, "[ Main ] Erreur fermeture Scheduler\n");
+	for(i = 0; i < nbFilters; i++){
+		if ((etat = pthread_join(idThdFil[i], NULL)) != 0)
+			fprintf(stderr, "[ Main ] Erreur fermeture Filter\n");
+	}
+	for(i = 0; i < nb_imprimantes_locale; i++){
+		if ((etat = pthread_join(idThdImp[i], NULL)) != 0)
+			fprintf(stderr, "[ Main ] Erreur fermeture Imprimante locale\n");
+	}
+	exit(0);
 }
